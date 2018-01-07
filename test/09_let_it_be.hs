374254fc811c8122fdd55593985a7bd6ad2644bf
Adam McCullough <merlinfmct87@gmail.com>
2018-01-07 02:33:51 -0800

    Kata number nine solution set.

--- test/09_let_it_be.hs
+++ test/09_let_it_be.hs
@@ -2,31 +2,35 @@ import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
 
-{- cylinder :: (RealFloat a) => a -> a -> a -}
-{- cylinder r h = -}
-    {- ... -}
-    {- ... -}
-    {- in sideArea + 2 * topArea -}
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea  = pi * r^2
+  in sideArea + (2 * topArea)
 
-{- cylinder' :: (RealFloat a) => a -> a -> a -}
-{- cylinder' r h = sideArea + 2 * topArea -}
-    {- where ... -}
-          {- ... -}
+cylinder' :: (RealFloat a) => a -> a -> a
+cylinder' r h = sideArea + 2 * topArea
+    where sideArea = 2 * pi * r * h
+          topArea  = pi * r^2
 
-{- calcBmis :: (RealFloat a) => [(a,a)] -> [a] -}
+calcBmis :: (RealFloat a) => [(a,a)] -> [a]
+calcBmis [] = []
+calcBmis ((w,h):xs) = bmi : calcBmis xs
+    where bmi = w / (h^2)
+
+
+square :: Num a => a -> a
+square a = a * a
 
 main :: IO()
 main = hspec $ do
     describe "Let bindings are almost like where" $ do
         it "can calculate cylinder surface area" $ do
-            pending
-            {- cylinder 1 2 `shouldBe` 18.84955592153876 -}
-            {- cylinder' 1 2 `shouldBe` 18.84955592153876 -}
+            cylinder 1 2 `shouldBe` 18.84955592153876
+            cylinder' 1 2 `shouldBe` 18.84955592153876
         it "is an expression in itself" $ do
-            pending
-            {- let result = [... in (square 5,square 3,square 2)] -}
-            {- result `shouldBe` [(25,9,4)] -}
+            let result = [(square 5,square 3,square 2)] in
+              result `shouldBe` [(25,9,4)]
         it "can be used in list comprehensions" $ do
-            pending
-            {- calcBmis [(80,1.9)] `shouldBe` [] -}
+            calcBmis [(80,1.9)] `shouldBe` [22.1606648199446]
 
